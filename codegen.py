f1e712972865a1e1fa14b929df379ea6d07a1b0a
Ehren Metcalfe <ehren.m@gmail.com>
2022-06-17 22:19:51 -0400

    slightly working codegen

--- codegen.py
+++ codegen.py
@@ -1,4 +1,6 @@
 from semanticanalysis import Node, Module, Call, Block, UnOp, BinOp, ColonBinOp, Assign, NamedParameter, Identifier, IntegerLiteral, IfNode, SemanticAnalysisError
+import io
+import textwrap
 
 cpp_preamble = """
 #include <memory>
@@ -12,69 +14,78 @@ class object {
 """
 
 
-import io
+def codegen(expr):
+    assert isinstance(expr, Module)
+    s = codegen_node(expr)
+    return cpp_preamble + s
 
-def codegen(parsed):
+
+def codegen_node(node: Node):
     cpp = io.StringIO()
-    cpp.write(cpp_preamble)
 
-
-
-    codegen_stack = []
-
-    #cpp.seek(io.SEEK_END)
-
-    assert isinstance(parsed, Module) # enforced by parser
-    codegen_node(parsed, cpp)
+    if isinstance(node, Module):
+        for modarg in node.args:
+            if modarg.func.name == "def":
+                defcode = codegen_def(modarg)
+                cpp.write(defcode)
+            else:
+                print("probably should handle", modarg)
+    elif isinstance(node, Call): # not at module level
+        if node.func.name == "if":
+            cpp.write(codegen_if(node))
+    elif isinstance(node, (Identifier, IntegerLiteral)):
+        cpp.write(str(node))
+    elif isinstance(node, BinOp):
+        cpp.write(codegen_node(node.args[0]) + node.func + codegen_node(node.args[1]))
 
     return cpp.getvalue()
 
 
-def codegen_node(node: Node, cpp):
-    if isinstance(node, Module):
-        for modarg in node.args:
-            if modarg.func.name == "def":
-                codegen_node(modarg, cpp)
-
-
-
-def codegen_if(ifcall : Call, cpp):
+def codegen_if(ifcall : Call):
     assert isinstance(ifcall, Call)
     assert ifcall.func.name == "if"
 
-    ifnode = IfNode(ifcall.func, ifnode.args)
+    ifnode = IfNode(ifcall.func, ifcall.args)
 
-    # ifargs = list(ifnode.args)
-    cond = ifnode.cond
-    thenblock = ifnode.thenblock
-    codegen_block(thenblock)
+    cpp = "if (" + codegen_node(ifnode.cond) + ") {\n"
+    cpp += codegen_block(ifnode.thenblock)
 
-    cond = codegen_node(cond, cpp)
+    for elifcond, elifblock in ifnode.eliftuples:
+        cpp += "} else if (" + codegen_node(elifcond) + ") {\n"
+        cpp += codegen_block(elifblock)
 
-    cpp.write("if (")
+    if ifnode.elseblock:
+        cpp += "} else {\n"
+        cpp += codegen_block(ifnode.elseblock)
+
+    cpp += "}"
+
+    return cpp
 
 
-
-    cpp.write("if (")
-
-
-def codegen_block(block: Block, cpp):
+def codegen_block(block: Block):
     assert isinstance(block, Block)
+    cpp = ""
     for b in block.args:
-        if isinstance(b, Call):
-            if b.func.name == "if":
-                codegen_if(b, cpp)
-        elif isinstance(b, BinOp):
-            pass
-        elif isinstance(b, UnOp):
-            pass
+        cpp += codegen_node(b) + ";\n"
+        # if isinstance(b, Call):
+        #     if b.func.name == "if":
+        #         cpp += codegen_if(b)
+        # elif isinstance(b, BinOp):
+        #     pass
+        # elif isinstance(b, UnOp):
+        #     pass
+    return cpp
 
-        print(b)
+
+def indent(text, amount, ch=' '):
+    return textwrap.indent(text, amount * ch)
 
 
-def codegen_def(defnode: Call, cpp):
+def codegen_def(defnode: Call):
     assert defnode.func.name == "def"
     name = defnode.args[0]
+    cpp = io.StringIO()
     cpp.write(f"std::shared_ptr<object> {name} (")
     args = defnode.args[1:]
     block = args.pop()
@@ -83,8 +94,9 @@ def codegen_def(defnode: Call, cpp):
         cpp_args = ", ".join([f"std::shared_ptr<object> {a}" for a in args])
         cpp.write(cpp_args)
     cpp.write(") {\n")
-    codegen_block(block, cpp)
+    cpp.write(codegen_block(block))
     cpp.write("}")
+    return cpp.getvalue()
 
 
 # class Environment:
