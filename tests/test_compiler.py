938dda8ff1cd61063218ea2d1c2de4ff73f7f076
Ehren Metcalfe <ehren.m@gmail.com>
2022-06-24 15:43:38 -0400

    actual unit tests

---
+++ tests/test_compiler.py
@@ -0,0 +1,34 @@
+from compiler import compile
+
+
+def test_stuff():
+
+    compile("""
+    
+def (map, values, fun:
+    results = [1]
+    results.append(fun(v))
+   # for v in values:
+   #     results.append(fun(v))
+   # return results
+    i = 0
+    x = i
+    i = x
+    (z = i+1)
+    (x+5)
+    (,)
+    (1,2)
+    (1,)
+    (1)
+    
+    while (i + 5:
+        results.append(fun(v))
+        results.pop()
+        pass
+    )
+    return results
+)
+    
+    """)
+
+
---
+++ tests/test_parser.py
@@ -0,0 +1,13 @@
+from parser import parse, TupleLiteral, Module
+
+
+def test_tuple():
+    m = parse("""
+(,)
+    """)
+    assert isinstance(m, Module)
+    t, = m.args
+    
+    assert isinstance(t, TupleLiteral)
+    assert t.args == []
+
