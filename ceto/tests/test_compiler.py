befdad9d3da47253aadd842dba8150958e6dc770
Ehren Metcalfe <ehren.m@gmail.com>
2023-05-09 14:58:10 -0400

    fix some WError failures with clang

--- ceto/tests/test_compiler.py
+++ ceto/tests/test_compiler.py
@@ -3226,15 +3226,15 @@ def (main:
     b = Blah()
     b.huh()  # auto unwrap via template magic
     b->huh() # no magic (but bad style for object derived class instances!)
-    printf("addr %p\n", (&b)->get()) # cheat the template magic to get the real shared_ptr (or rather a pointer to it)
+    printf("addr %p\n", static_cast<const:void:ptr>((&b)->get())) # cheat the template magic to get the real shared_ptr (or rather a pointer to it)
     # printf("addr %p\n", (&b).get()) # woo hoo this no longer works! (we previously autoderefed _every_ pointer on every attribute access...)
     # printf("addr temp %p\n", (&Blah())->get())  #  error: taking the address of a temporary object of type 'typename enable_if<!is_array<Blah> ... - good error
     # printf("use_count %ld\n", (&b).use_count())  # no autoderef for arbitrary pointers!
     printf("use_count %ld\n", (&b)->use_count())
     b_addr = &b
-    printf("addr of shared_ptr instance %p\n", b_addr)
+    printf("addr of shared_ptr instance %p\n", static_cast<const:void:ptr>(b_addr))
     # printf("addr %p\n", b_addr.get()) # correct error
-    printf("addr %p\n", b_addr->get())
+    printf("addr %p\n", static_cast<const:void:ptr>(b_addr->get()))
     # printf("use_count %ld\n", b_addr.use_count()) # correct error
     printf("use_count %ld\n", b_addr->use_count())
     (*b_addr).huh() # note *b_addr is a shared_ptr<Blah> so accessing via '.' already does deref magic (it's fine)
@@ -3266,11 +3266,11 @@ class (Uniq:
 
     def (bar: mut:
         self.x = self.x + 1
-        printf("in bar %d %p\n", self.x, this)
+        printf("in bar %d %p\n", self.x, static_cast<const:void:ptr>(this))
         return self.x
         # just ensure the manual approach compiles too (unreachable):
         this->x = this->x + 1
-        printf("in bar %d %p\n", this->x, this)
+        printf("in bar %d %p\n", this->x, static_cast<const:void:ptr>(this))
         return this->x
     )
 ): unique
@@ -3914,7 +3914,7 @@ class (Foo: # this is the parse prob
     # )
 
     def (destruct:
-        printf("dead %p\n", this)
+        printf("dead %p\n", static_cast<const:void:ptr>(this))
     )
 
     def (bar:
@@ -3922,7 +3922,7 @@ class (Foo: # this is the parse prob
     )
 
     def (foo:
-        printf("in foo method %p\n", this)
+        printf("in foo method %p\n", static_cast<const:void:ptr>(this))
 
         bar()
         self.bar()
