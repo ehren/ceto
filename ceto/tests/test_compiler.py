1da755defb681e2e4d53530340628590b8d91c65
Ehren Metcalfe <ehren.m@gmail.com>
2023-09-12 11:45:32 -0400

    try to create a use after free using static vars, aliasing, and passing by const ref. not successful?

--- ceto/tests/test_compiler.py
+++ ceto/tests/test_compiler.py
@@ -19,6 +19,74 @@ def raises(func, exc=None):
     else:
         assert 0
 
+def test_alias1():
+    # this should be fine too
+    c = compile(r"""
+class (Foo:
+    def (destruct:
+        std.cout << "Foo destruct"
+    )
+)
+
+def (aliaser, f : mut:auto:ref, g:
+    std.cout << "in aliaser"
+    f = nullptr
+    std.cout << (&g)->use_count()  # 0
+    std.cout << ((&g)->get() == nullptr)  # 1
+    std.cout << (g == nullptr)  # 1
+)
+
+def (main:
+    f : mut = Foo()
+    aliaser(f, f)
+)
+    """)
+
+    assert c == "in aliaserFoo destruct011"
+
+
+def test_attempt_use_after_free():
+    c = compile(r"""
+class (Foo:
+    def (method:
+        std.cout << "method"
+    )
+
+    def (destruct:
+        std.cout << "Foo destruct"
+    )
+)
+    
+class (Holder:
+    f : Foo
+)
+
+def (accessor:
+    g : mut:static:auto = Holder(nullptr)  # TODO we don't support the arguably more sensible 'static:mut:auto' (fine for now)
+    
+    return g
+)
+
+def (aliaser, f:
+    g = accessor()
+    g.f = nullptr
+    # f.method()  # this would raise (good)
+    std.cout << (f == nullptr)  # true - still safe at this point? (reference lifetime extension for the shared_ptr itself (not what it's pointing to) to then end of main?)
+    std.cout << (&f)->use_count()  # 0 
+    std.cout << ((&f)->get() == nullptr) # 1
+)
+
+def (main:
+    f = accessor()
+    f.f = Foo()  # + 1
+    std.cout << (&(f.f))->use_count()
+    aliaser(f.f) # + 0  (passed by const ref...)
+)
+    
+    """)
+
+    assert c == "1Foo destruct101"
+
 
 def test_bug_constructor_param_should_be_ptr_to_mut_not_const():
     c = compile(r"""
