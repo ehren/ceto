843338616601f7aaccbc6fad63dfc29489c5ad89
Ehren Metcalfe <ehren.m@gmail.com>
2023-10-01 15:33:03 -0400

    misc test additions

--- ceto/tests/test_compiler.py
+++ ceto/tests/test_compiler.py
@@ -29,21 +29,79 @@ struct (Foo:
     x : string
 )
 
-def (foo, f : Foo:
+def (by_const_ref, f : Foo:  # pass by const ref
     static_assert(std.is_same_v<decltype(f), const:Foo:ref>)
     static_assert(std.is_reference_v<decltype(f)>)
     static_assert(std.is_const_v<std.remove_reference_t<decltype(f)>>)
     std.cout << f.x
 )
 
+def (by_val, f: Foo:mut:  # pass by value
+    static_assert(std.is_same_v<decltype(f), Foo>)
+    static_assert(not std.is_reference_v<decltype(f)>)
+    static_assert(not std.is_const_v<std.remove_reference_t<decltype(f)>>)
+    std.cout << f.x
+)
+
+def (by_const_val, f: Foo:const:  # pass by const value (TODO perhaps this should be pass by const ref instead - bit of a perf gotcha. same problem with string/std.string and [T])
+    static_assert(std.is_same_v<decltype(f), const:Foo>)
+    static_assert(not std.is_reference_v<decltype(f)>)
+    static_assert(std.is_const_v<std.remove_reference_t<decltype(f)>>)
+    std.cout << f.x
+)
+
+def (by_mut_ref, f: Foo:ref:mut:  # pass by non-const reference
+    static_assert(std.is_same_v<decltype(f), Foo:ref>)
+    static_assert(std.is_reference_v<decltype(f)>)
+    static_assert(not std.is_const_v<std.remove_reference_t<decltype(f)>>)
+    f.x += "hi"
+    std.cout << f.x
+)
+
+def (by_ptr, f: Foo:ptr:  # TODO - our add const for function params with raw pointers should behave like the shared_ptr case. That is, this should really be a const Foo *const - we're generating a const Foo*  (not that it's recommended to use raw pointers anywhere)
+    # static_assert(std.is_same_v<decltype(f), const:Foo:ptr:const>)  # TODO this should work
+    static_assert(std.is_same_v<decltype(f), const:Foo:ptr>)  # TODO this is wrong
+    static_assert(not std.is_reference_v<decltype(f)>)
+    static_assert(not std.is_const_v<std.remove_reference_t<decltype(f)>>)  # TODO this is wrong
+    static_assert(std.is_const_v<std.remove_reference_t<decltype(*f)>>)  # this is right at least
+    std.cout << f->x
+)
+
+def (by_ptr_all_const, f: const:Foo:ptr:const:  # TODO this is what we want for Foo:ptr 
+    # static_assert(std.is_same_v<decltype(f), const:Foo:ptr:const>)
+    static_assert(not std.is_reference_v<decltype(f)>)
+    static_assert(std.is_const_v<std.remove_reference_t<decltype(f)>>)
+    static_assert(std.is_const_v<std.remove_reference_t<decltype(*f)>>)
+    std.cout << f->x
+)
+
+def (by_ptr_mut, f: Foo:ptr:mut:
+    static_assert(std.is_same_v<decltype(f), Foo:ptr>)
+    static_assert(not std.is_reference_v<decltype(f)>)
+    static_assert(not std.is_const_v<std.remove_reference_t<decltype(f)>>)
+    static_assert(not std.is_const_v<std.remove_reference_t<decltype(*f)>>)
+    f->x += "viaptr"
+    std.cout << f->x
+)
+
 def (main:
-    foo(Foo("blah"))
-    # foo(Foo("blah") : mut)  # TODO this should be an error not a silent 'mut' strip
-                              # Foo("blah") : mut:shared or Foo("blah") : shared:mut should be allowed
+    f = Foo("blah")
+    by_const_ref(f)
+    by_val(f)
+    by_const_val(f)
+    fm : mut = f  # copy
+    # by_mut_ref(f)  # error: binding reference of type ‘Foo&’ to ‘const Foo’ discards qualifiers
+    by_mut_ref(fm)
+    by_ptr(&f)
+    by_ptr_all_const(&f)
+    by_ptr_mut(&fm)
+    
+    # by_const_ref(Foo("blah") : mut)  # TODO this should be an error not a silent 'mut' strip
+                                       # Foo("blah") : mut:shared or Foo("blah") : shared:mut should be allowed
 )
     """)
 
-    assert c == "blah"
+    assert c == "blahblahblahblahhiblahblahblahhiviaptr"
 
 
 
@@ -66,35 +124,35 @@ pmfn = &Testpm.func1
 pmd = &Testpm.num
 
 def (main:
-    ATestpm = *Testpm(5)
-    p_Testpm = &ATestpm
+    a_Testpm = *Testpm(5)
+    p_Testpm = &a_Testpm
     
-    ATestpm_mut : mut = *Testpm(5)
-    p_Testpm_mut : mut = &ATestpm_mut
+    a_Testpm_mut : mut = *Testpm(5)
+    p_Testpm_mut : mut = &a_Testpm_mut
     
-    # (ATestpm.*pmfn)()    # fails to parse
+    # (a_Testpm.*pmfn)()    # fails to parse
     # (pTestpm->*pmfn)()   # in c++: Parentheses required since * binds less tightly than the function call. (fails to parse)
 
-    # ATestpm.*pmd = 1  # fails to parse
-    # pTestpm->*pmd = 2 # fails to parse
+    # a_Testpm.*pmd = 1  # fails to parse
+    # p_Testpm->*pmd = 2 # fails to parse
     
-    # (ATestpm.(*pmfn))()  # parsing succeeds but fails in c++ (note autoderef here too)
+    # (a_Testpm.(*pmfn))()  # parsing succeeds but fails in c++ (note autoderef here too)
     # : error: invalid use of unary ‘*’ on pointer to member
-    #          ceto::mado(ATestpm)->(*pmfn)();
+    #          ceto::mado(a_Testpm)->(*pmfn)();
     
     # (p_Testpm->(*pmfn))()  # likewise fails in c++
     # error: invalid use of unary ‘*’ on pointer to member
     #    43 |         p_Testpm -> (*pmfn)();
     
-    MFPTR_DOT(ATestpm, pmfn)()
+    MFPTR_DOT(a_Testpm, pmfn)()
     MFPTR_ARROW(p_Testpm, pmfn)()
     
-    # MFPTR_DOT(ATestpm, pmd) = 1  # error assignment to read only location
-    MFPTR_DOT(ATestpm_mut, pmd) = 1
+    # MFPTR_DOT(a_Testpm, pmd) = 1  # error assignment to read only location
+    MFPTR_DOT(a_Testpm_mut, pmd) = 1
     MFPTR_ARROW(p_Testpm_mut, pmd) = 2
     
-    std.cout << MFPTR_DOT(ATestpm, pmd) << MFPTR_ARROW(p_Testpm, pmd)
-    std.cout << MFPTR_DOT(ATestpm_mut, pmd) << MFPTR_ARROW(p_Testpm_mut, pmd)
+    std.cout << MFPTR_DOT(a_Testpm, pmd) << MFPTR_ARROW(p_Testpm, pmd)
+    std.cout << MFPTR_DOT(a_Testpm_mut, pmd) << MFPTR_ARROW(p_Testpm_mut, pmd)
 )
     """)
 
@@ -668,7 +726,7 @@ def (main:
     if (not defined(_MSC_VER):
         l6 = lambda(x:int, lambda(y, y + x):int)(2)(3)
     else:
-        l6 = lambda(x:int, return lambda(y, y + x):int)(2)(3)  # something about the 'is void?' constexpr if (for implicit return in lambda) doesn't even parse in msvc (their bug)
+        l6 = lambda(x:int, return lambda(y, y + x):int)(2)(3)  # something about the double use of 'is void?' constexpr if (for implicit return in lambda) doesn't even parse in msvc (their bug)
     ) : pre
     std.cout << l6
     if (not defined(_MSC_VER):
