86bd2ea14e312af33ab83382e6090d8382712376
Ehren Metcalfe <ehren.m@gmail.com>
2023-10-28 14:37:26 -0400

    add traversal of child nodes to ast visitor example

--- tests/ast_visitor.ctp
+++ tests/ast_visitor.ctp
@@ -2,10 +2,14 @@
 # Test Output: visiting Node
 # Test Output: recording Ident visit
 # Test Output: recording Node visit
+# Test Output: recording Ident visit
+# Test Output: recording Ident visit
+# Test Output: recording Ident visit
 
 
 class (Node)
 class (Identifier)
+class (ListLiteral)
 
 
 class (BaseVisitor:
@@ -16,20 +20,14 @@ class (BaseVisitor:
     def (visit: virtual:mut, ident: Identifier:
         std.cout << "visiting Identifier\n"
     )
-)
 
-class (RecordingVisitor(BaseVisitor):
-    record = ""
-
-    def (visit: virtual:mut, node: Node:
-        self.record += "recording Node visit\n"
-    )
-
-    def (visit: virtual:mut, ident: Identifier:
-        self.record += "recording Ident visit\n"
+    def (visit: virtual:mut, list_literal: ListLiteral:
+        std.cout << "visiting ListLiteral\n"
+        # here we're choosing not to visit the children of list_literal to
+        # contrast with the behavior of RecordingVisitor
+        # (and to avoid needing to define currently unsupported out of line methods...)
     )
 )
-
 
 class (Node:
 
@@ -50,17 +48,57 @@ class (Identifier(Node):
     )
 )
 
+class (ListLiteral(Node):
+    args : [Node]
+
+    def (init, args:
+        self.args = args
+    )
+
+    def (accept: virtual, visitor: BaseVisitor:mut:
+        visitor.visit(self)
+    )
+)
+
+
+class (RecordingVisitor(BaseVisitor):
+    record = ""
+
+    def (visit: virtual:mut, node: Node:
+        self.record += "recording Node visit\n"
+    )
+
+    def (visit: virtual:mut, ident: Identifier:
+        self.record += "recording Ident visit\n"
+    )
+
+    def (visit: virtual:mut, list_literal: ListLiteral:
+        std.cout << "visiting ListLiteral\n"
+
+        # in contrast to BaseVisitor, here we choose to traverse the children
+        for (arg in list_literal.args:
+            arg.accept(self)
+        )
+    )
+)
+
 
 def (main:
     node = Node()
     ident = Identifier("a")
 
-    visitor: mut = BaseVisitor()
-    ident.accept(visitor)
-    node.accept(visitor)
+    simple_visitor: mut = BaseVisitor()
+    ident.accept(simple_visitor)
+    node.accept(simple_visitor)
 
     recording_visitor: mut = RecordingVisitor()
     ident.accept(recording_visitor)
     node.accept(recording_visitor)
+
+    list_args : [Node] = [ident, ident, ident]
+    list_literal = ListLiteral(list_args)
+    list_literal.accept(simple_visitor)
+    list_literal.accept(recording_visitor)
+
     std.cout << recording_visitor.record
 )
--- tests/ast_visitor.donotedit.autogenerated.cpp
+++ tests/ast_visitor.donotedit.autogenerated.cpp
@@ -22,6 +22,8 @@ class Node;
 
 class Identifier;
 
+class ListLiteral;
+
 struct BaseVisitor : ceto::shared_object {
 
          virtual inline auto visit(const std::shared_ptr<const Node>&  node) -> void {
@@ -32,20 +34,8 @@ struct BaseVisitor : ceto::shared_object {
             std::cout << std::string {"visiting Identifier\n"};
         }
 
-};
-
-struct RecordingVisitor : public BaseVisitor {
-
-using BaseVisitor::BaseVisitor;
-
-    std::remove_cvref_t<decltype(std::string {""})> record = std::string {""};
-
-         virtual inline auto visit(const std::shared_ptr<const Node>&  node) -> void {
-            (this -> record) += std::string {"recording Node visit\n"};
-        }
-
-         virtual inline auto visit(const std::shared_ptr<const Identifier>&  ident) -> void {
-            (this -> record) += std::string {"recording Ident visit\n"};
+         virtual inline auto visit(const std::shared_ptr<const ListLiteral>&  list_literal) -> void {
+            std::cout << std::string {"visiting ListLiteral\n"};
         }
 
 };
@@ -75,15 +65,57 @@ struct Identifier : public Node {
 
 };
 
+struct ListLiteral : public Node {
+
+    std::vector<std::shared_ptr<const Node>> args;
+
+         virtual inline auto accept(const std::shared_ptr<BaseVisitor>&  visitor) const -> void {
+            const auto self = ceto::shared_from(this);
+            ceto::mado(visitor)->visit(self);
+        }
+
+    explicit ListLiteral(const std::vector<std::shared_ptr<const Node>>&  args) : args(args) {
+    }
+
+    ListLiteral() = delete;
+
+};
+
+struct RecordingVisitor : public BaseVisitor {
+
+using BaseVisitor::BaseVisitor;
+
+    std::remove_cvref_t<decltype(std::string {""})> record = std::string {""};
+
+         virtual inline auto visit(const std::shared_ptr<const Node>&  node) -> void {
+            (this -> record) += std::string {"recording Node visit\n"};
+        }
+
+         virtual inline auto visit(const std::shared_ptr<const Identifier>&  ident) -> void {
+            (this -> record) += std::string {"recording Ident visit\n"};
+        }
+
+         virtual inline auto visit(const std::shared_ptr<const ListLiteral>&  list_literal) -> void {
+            std::cout << std::string {"visiting ListLiteral\n"};
+            for(const auto& arg : ceto::mado(list_literal)->args) {
+                this -> visit(arg);
+            }
+        }
+
+};
+
     auto main() -> int {
         const auto node = std::make_shared<const decltype(Node())>();
         const auto ident = std::make_shared<const decltype(Identifier{std::string {"a"}})>(std::string {"a"});
-        auto visitor { std::make_shared<decltype(BaseVisitor())>() } ;
-        ceto::mado(ident)->accept(visitor);
-        ceto::mado(node)->accept(visitor);
+        auto simple_visitor { std::make_shared<decltype(BaseVisitor())>() } ;
+        ceto::mado(ident)->accept(simple_visitor);
+        ceto::mado(node)->accept(simple_visitor);
         auto recording_visitor { std::make_shared<decltype(RecordingVisitor())>() } ;
         ceto::mado(ident)->accept(recording_visitor);
         ceto::mado(node)->accept(recording_visitor);
+        const auto list_literal = std::make_shared<const decltype(ListLiteral{std::vector {{ident, ident, ident}}})>(std::vector {{ident, ident, ident}});
+        ceto::mado(simple_visitor)->accept(list_literal);
+        ceto::mado(recording_visitor)->accept(list_literal);
         std::cout << ceto::mado(recording_visitor)->record;
     }
 
