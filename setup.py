1f5f648e00c64568d391f6fa03a2043d991a537d
Ehren Metcalfe <ehren.m@gmail.com>
2026-01-18 23:29:30 -0500

    not_null wrapper from gsl

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ manifest = os.path.join(rootdir, "MANIFEST.in")
 
 packaged_include_ceto = os.path.join(rootdir, "ceto", "ceto.h")
 packaged_include_propconst = os.path.join(rootdir, "ceto", "propagate_const_copyable.h")
+packaged_include_not_null = os.path.join(rootdir, "ceto", "not_null.h")
 packaged_ast_header = os.path.join(rootdir, "ceto", "ast.cth")
 packaged_utility_header = os.path.join(rootdir, "ceto", "utility.cth")
 packaged_range_utility_header = os.path.join(rootdir, "ceto", "range_utility.cth")
@@ -116,6 +117,7 @@ for f in os.listdir(os.path.join(rootdir, "include")):
 
 shutil.copyfile(os.path.join(rootdir, "include", "ceto.h"), packaged_include_ceto)
 shutil.copyfile(os.path.join(rootdir, "include", "propagate_const_copyable.h"), packaged_include_propconst)
+shutil.copyfile(os.path.join(rootdir, "include", "not_null.h"), packaged_include_not_null)
 shutil.copyfile(os.path.join(rootdir, "selfhost", "ast.cth"), packaged_ast_header)
 shutil.copyfile(os.path.join(rootdir, "selfhost", "utility.cth"), packaged_utility_header)
 shutil.copyfile(os.path.join(rootdir, "selfhost", "range_utility.cth"), packaged_range_utility_header)
